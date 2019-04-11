9c3baa4c51c8cce01fac69a7b1a1a65d522de8c8
stathisKara <sdi1200057@di.uoa.gr>
2019-04-11 16:46:11 -0400

    Updates course class wtih all fields, dummy course creation and comments

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,10 +1,14 @@
 package gui;
 
-public class Main {
+import model.Course;
 
+public class Main {
+	
 	public static void main(String[] args) {
 		// GUI is initialized here
-		
+		Course c = new Course();
+		c.addCategory(0.5, "dummy2", false);
+		System.out.println(c.getAllCategories().get(1));
 	}
-
+	
 }
--- src/model/Course.java
+++ src/model/Course.java
@@ -1,16 +1,135 @@
 package model;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
 import java.util.ArrayList;
 
 public class Course {
+	
+	//------------ Course charateristics ------------
 	private String name;
 	private String code;
-	private ArrayList<Caterogy> categories;
-	private ArrayList <Student> students;
-	private Summary s;
+	private int year;
+	private boolean status;
 	
-	public Course(String name, String code) {
+	//------------ Course contents ------------
+	private ArrayList<Category> categories;
+	private ArrayList<Student> students;
+	private Summary summary;
+	
+	
+	//------------ Constructors ------------
+	public Course(String name, String code, int year) {
 		this.name = name;
 		this.code = code;
+		this.year = year;
+		categories = new ArrayList<>();
+		students = new ArrayList<>();
+		summary = new Summary();
+		status = true;
+	}
+	
+	public Course() {
+		this("dummy", "dummy", 0);
+		
+		categories.add(new GradeableCategory());
+		Student student = new Student();
+		students.add(student);
+		Component component = new GradableComponent("dummy", true);
+		categories.get(0).addComponent(students, component);
+		categories.get(0).addStudentEntry(student);
+	}
+	
+	//------------ Characteristics Getters & Setters ------------
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public boolean status() {
+		return status;
+	}
+	
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public ArrayList<Category> getAllCategories() {
+		return this.categories;
+	}
+	
+	public Category getCategory(int index) {
+		return categories.get(index);
+	}
+	
+	public Summary getSummary() {
+		return this.summary;
+	}
+	
+	
+	//------------ Methods for adding a category ------------
+	public void addCategory(double weight, String name, boolean isGradeable) {
+		if (isGradeable)
+			addGradeable(weight, name);
+		else
+			addNonGradeable(name);
+	}
+	
+	private void addGradeable(double weight, String name) {
+		categories.add(new GradeableCategory(weight, name));
+	}
+	
+	private void addNonGradeable(String name) {
+		categories.add(new TextCategory(name));
+	}
+	
+	public void deleteCategory(String name) {
+		for (Category categ : categories) {
+			if (categ.getName().equals(name)) {
+				categories.remove(categ);
+				break;
+			}
+		}
+	}
+	
+	//------------- Methods for adding students ------------
+	public void addStudent(String sId, String fName, String mName, String lName) {
+		Student student = new Student(sId, fName, mName, lName);
+		students.add(student);
+		for (Category category : categories) {
+			category.addStudentEntry(student);
+		}
+	}
+	
+	public void bulkLoadStudents(String csvName) {
+	
+	}
+	
+	public void deleteStudent(String sId) {
+		for (Student stud : students) {
+			if (stud.getSid().equals(sId)) {
+//				categories.deleteStudent(students.indexOf(stud));
+				students.remove(stud);
+			}
+		}
 	}
 }
