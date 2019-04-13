50853578591704bf264620a5767f1a76c886bd7a
stathisKara <sdi1200057@di.uoa.gr>
2019-04-13 12:53:32 -0400

    Updated category

--- src/model/Category.java
+++ src/model/Category.java
@@ -7,20 +7,64 @@ abstract class Category {
 	protected ArrayList<Component> components;
 	protected ArrayList<StudentEntry> studentEntries;
 	
+	
 	public Category(String categName) {
 		this.name = categName;
 		components = new ArrayList<>();
 		studentEntries = new ArrayList<>();
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void addStudentEntry(Student student){
+		studentEntries.add(new StudentEntry(student));
+	}
+	
+	public void addComponent(ArrayList<Student> students, Component component){
+		components.add(component);
+		for (Student student : students){
+			addStudentEntry(student);
+		}
+	}
 }
 
 class TextCategory extends Category {
+	private String content;
+	
+	public TextCategory() {
+		this("Dummy");
+	}
+	
 	public TextCategory(String categName) {
 		super(categName);
+		content = "";
+	}
+	
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "TextCategory{" +
+				"name='" + name + '\'' +
+				", content='" + content + '\'' +
+				'}';
 	}
 }
 
 class Summary extends TextCategory {
+	public Summary() {
+		this("Summary");
+	}
+	
 	public Summary(String categName) {
 		super(categName);
 	}
@@ -28,6 +72,10 @@ class Summary extends TextCategory {
 
 class GradeableCategory extends Category {
 	double weight;
+	
+	public GradeableCategory() {
+		this(0.5, "dummy");
+	}
 	
 	public GradeableCategory(double weight, String categName) {
 		super(categName);
@@ -44,6 +92,6 @@ class GradeableCategory extends Category {
 	
 	@Override
 	public String toString() {
-		return weight + "%";
+		return this.name + " " + this.weight + "%";
 	}
 }
