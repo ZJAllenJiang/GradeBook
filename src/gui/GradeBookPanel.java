73aa45a5f1298f1e4bf543b9f40f73591933b595
aburkato <aburkatovskiy@gmail.com>
2019-04-14 17:59:55 -0400

    Added right click menu items to the tab of the grade book.

--- src/gui/GradeBookPanel.java
+++ src/gui/GradeBookPanel.java
@@ -1,12 +1,21 @@
 package gui;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+import javax.swing.plaf.TabbedPaneUI;
 
 import model.Category;
 import model.Course;
+import model.Summary;
 
 public class GradeBookPanel extends JPanel {
 	private Course course;
@@ -23,12 +32,60 @@ public class GradeBookPanel extends JPanel {
 		gradeBookTabs = new JTabbedPane();
 		
 		//Add the Summary
-		gradeBookTabs.addTab("Summary", new GradeBookTablePanel(course.getSummary()));
+		gradeBookTabs.addTab(Summary.SUMMARY, new GradeBookTablePanel(course.getSummary()));
 		
 		//Add the other Categories
 		for(Category category : course.getAllCategories()) {
 			gradeBookTabs.addTab(category.getName(), new GradeBookTablePanel(category));
 		}
+		
+		gradeBookTabs.addMouseListener(new MouseAdapter() {
+		    @Override
+		    public void mouseClicked(MouseEvent e) 
+		    {
+		    	if(SwingUtilities.isRightMouseButton(e)){
+		    		TabbedPaneUI tabUI = gradeBookTabs.getUI();
+		    		int tabIndex = tabUI.tabForCoordinate(gradeBookTabs, e.getX(), e.getY());
+		    		if(tabIndex >= 0) {
+						String tabName = gradeBookTabs.getTitleAt(tabIndex);
+						//Can't modify Summary tab
+						if(!tabName.equals(Summary.SUMMARY)) {
+			    			JPopupMenu popupMenu = new JPopupMenu();
+			    			
+			    			//Add menu items
+			    			JMenuItem statisticItem = new JMenuItem("Compute Statistics");
+			    			statisticItem.addActionListener(new ActionListener() {
+			    	            @Override
+			    	            public void actionPerformed(ActionEvent e) {
+			    	            	System.out.println("Compute statistics on column: " + tabName);
+			    	            }
+			    	        });
+			    	        popupMenu.add(statisticItem);
+			    			
+			    			JMenuItem editItem = new JMenuItem("Edit");
+			    			editItem.addActionListener(new ActionListener() {
+			    	            @Override
+			    	            public void actionPerformed(ActionEvent e) {
+			    	            	System.out.println("Do edit on column: " + tabName);
+			    	            }
+			    	        });
+			    	        popupMenu.add(editItem);
+			    	        
+			    	        JMenuItem deleteItem = new JMenuItem("Delete");
+			    	        deleteItem.addActionListener(new ActionListener() {
+			    	            @Override
+			    	            public void actionPerformed(ActionEvent e) {
+			    	            	System.out.println("Do delete on column: " + tabName);
+			    	            }
+			    	        });
+			    	        popupMenu.add(deleteItem);
+			    			
+			    	        popupMenu.show(gradeBookTabs, e.getX(), e.getY());
+						}
+		    		}
+		        }
+		    }
+		});
 		
 		add(gradeBookTabs);
 	}
