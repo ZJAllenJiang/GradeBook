0a082cef145b4663d306256d504dba2e37f4596f
aburkato <aburkatovskiy@gmail.com>
2019-04-16 16:48:24 -0400

    Have two add column buttons for a gradeable category.

--- src/gui/GradeBookTablePanel.java
+++ src/gui/GradeBookTablePanel.java
@@ -1,22 +1,26 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import model.Category;
 import model.CategoryComponent;
+import model.GradeableComponent;
+import model.GradeableComponent.DataEntryMode;
 import model.TextComponent;
 
 
 public class GradeBookTablePanel extends JPanel {
 	private Category category;
 	
-	private JButton addComponentBtn;
 	private GradeBookJTable gradeBookTable;
 	
 	public GradeBookTablePanel(Category category) {
@@ -33,17 +37,32 @@ public class GradeBookTablePanel extends JPanel {
 
 	private void addButtonsForTable() {
 		JPanel addPanel = new JPanel();
-		addComponentBtn = new JButton("Add New Column");
-		addComponentBtn.addActionListener(new ActionListener() {
+		if(category.isGradeable()) {
+			JButton addGradeBtn = new JButton("Add New " + category.getName());
+			addGradeBtn.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("Added new column to " + category.getName());
+					CategoryComponent c = new GradeableComponent("Grade", true,
+							.1, 50, DataEntryMode.POINTS_EARNED);
+					category.addComponent(c);
+					gradeBookTable.refreshTable();
+				}
+			});
+			addPanel.add(addGradeBtn);
+			addPanel.add(Box.createRigidArea(new Dimension(50, 0)));	
+		}
+		JButton addTextBtn = new JButton("Add New Text Column");
+		addTextBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Added new column to " + category.getName());
-				CategoryComponent c = new TextComponent("Test", true);
+				CategoryComponent c = new TextComponent("Text", true);
 				category.addComponent(c);
 				gradeBookTable.refreshTable();
 			}
 		});
-		addPanel.add(addComponentBtn);
+		addPanel.add(addTextBtn);
 		this.add(addPanel, BorderLayout.PAGE_START);
 	}
 	
