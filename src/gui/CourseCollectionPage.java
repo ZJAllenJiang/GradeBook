4fd8996607dc646d8d0c7a25a7f1758e69631640
ZJAllenJiang <weijiang@bu.edu>
2019-04-21 13:50:50 -0400

    Update gui page

--- src/gui/CourseCollectionPage.java
+++ src/gui/CourseCollectionPage.java
@@ -12,16 +12,22 @@ import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
+import data.DatabaseAPI;
+import model.Course;
 
 import java.awt.ScrollPane;
+import java.awt.Toolkit;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
 
 public class CourseCollectionPage {
 	public static JTable courseTable;
@@ -57,14 +63,25 @@ public class CourseCollectionPage {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setVisible(true);
-		frame.setBounds(100, 100, 600, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 800, 500);
+		//frame.setLocationRelativeTo(null);
+		// set the window in the middle of screen
+		int windowWidth = frame.getWidth(); 
+		int windowHeight = frame.getHeight(); 
+		Toolkit kit = Toolkit.getDefaultToolkit(); 
+		Dimension screenSize = kit.getScreenSize(); 
+		int screenWidth = screenSize.width; 
+		int screenHeight = screenSize.height; 
+		frame.setLocation(screenWidth / 2 - windowWidth / 2, screenHeight / 2 - windowHeight / 2);
+				
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setTitle("Course Collection");
 		
 		JLabel courseLabel = new JLabel("Courses");
 		courseLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		courseLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
-		courseLabel.setBounds(240, 17, 120, 60);
+		courseLabel.setBounds(340, 20, 120, 60);
 		frame.getContentPane().add(courseLabel);
 		
 		JButton addCourseButton = new JButton("Add Course +");
@@ -75,7 +92,7 @@ public class CourseCollectionPage {
 			}
 		});
 		addCourseButton.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
-		addCourseButton.setBounds(225, 70, 150, 35);
+		addCourseButton.setBounds(325, 90, 150, 35);
 		frame.getContentPane().add(addCourseButton);
 		
 		JButton logoutButton = new JButton("Logout");
@@ -89,13 +106,26 @@ public class CourseCollectionPage {
 		frame.getContentPane().add(logoutButton);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(25, 110, 550, 250);
+		scrollPane.setBounds(50, 150, 700, 300);
 		frame.getContentPane().add(scrollPane);
 		
 		String[] columnName = {"Name", "Code", "Year", "Semester", "Status", "Action1", "Action2", "Action3"};
-		Object[][] courseInfo= {
-				{"Java Development", "CS591", "2019", "Spring", "Active", "View", "Edit", "Delete"}
-		};
+//		Object[][] courseInfo= {
+//				{"Java Development", "CS591", "2019", "Spring", "Active", "View", "Edit", "Delete"}
+//		};
+		//DatabaseAPI.saveCourse(new Course("Data Mechanics", "504", 2019));
+		ArrayList<Course> courseList = DatabaseAPI.getCourseList();
+		Object[][] courseInfo = new Object[courseList.size()][8];
+		for (int i = 0; i < courseList.size(); i++) {
+			courseInfo[i][0] = courseList.get(i).getName();
+			courseInfo[i][1] = courseList.get(i).getCode();
+			courseInfo[i][2] = String.valueOf(courseList.get(i).getYear());
+			courseInfo[i][3] = "Spring";
+			courseInfo[i][4] = "Active";
+			courseInfo[i][5] = "View";
+			courseInfo[i][6] = "Edit";
+			courseInfo[i][7] = "Delete";
+		}
 		DefaultTableModel model = new DefaultTableModel(courseInfo, columnName);
 		courseTable = new JTable(courseInfo, columnName) {
 			public boolean isCellEditable(int row, int column) {
