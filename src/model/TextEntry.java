d5d7df635ee6b043df46f25721ee327c61e47868
simon wu‘ <simonwu@dhcp-wifi-8021x-155-41-43-251.bu.edu>
2019-04-27 10:58:46 -0400

    fix bugs of setting data

--- src/model/TextEntry.java
+++ src/model/TextEntry.java
@@ -15,4 +15,17 @@ public class TextEntry extends DataEntry<String>{
 		this.setData(guiData);
 		return true;
 	}
+	
+	@Override
+	public void setDataWithString(String xdata) {
+		try {
+			
+			// read String from database
+			// the real number of value so don't need to check entry mode and convert
+			this.setData(xdata);
+			
+		}catch(Exception e) {
+			System.out.println("[GradeEntry setDataWithString] " + e.getMessage());
+		}
+	}
 }
